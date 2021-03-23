d16b379d17aa446e86fe784b0daff2ede580fdc5
TGiaimo <tgiaimo1@gmail.com>
2021-03-22 21:49:16 -0400

    fixed MustangBronco.java and deleted MustangBravoGUI.java

--- src/main/MustangBronco.java
+++ src/main/MustangBronco.java
@@ -1,5 +1,9 @@
 package main;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MustangBronco {
   private int num;
 
@@ -20,4 +24,31 @@ public class MustangBronco {
   public void setNum(int num) {
     this.num = num;
   }
+  
+  public static class MustangBroncoGUI {
+    private JButton calculateButton;
+    private JTextField enterNumberTextField;
+    private JTextField textField2;
+    private JPanel panel;
+    private MustangBronco mustangBronco;
+
+    public MustangBroncoGUI() {
+      mustangBronco = new MustangBronco();
+      calculateButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          mustangBronco.setNum(Integer.parseInt(enterNumberTextField.getText()));
+          textField2.setText(mustangBronco.calculateMustangBronco());
+        }
+      });
+    }
+  }
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("MustangBroncoGUI");
+    frame.setContentPane(new MustangBroncoGUI().panel);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+  }
 }
+
